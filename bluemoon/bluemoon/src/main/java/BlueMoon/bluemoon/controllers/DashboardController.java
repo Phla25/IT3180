26040c3a642e7c7fb1961c446f187a769d8eb9a4
nguyenLTpk <nguyenngungonvc@gmail.com>
2025-10-20 12:14:43 +0700

    Thêm fee, profile

--- bluemoon/bluemoon/src/main/java/BlueMoon/bluemoon/controllers/DashboardController.java
+++ bluemoon/bluemoon/src/main/java/BlueMoon/bluemoon/controllers/DashboardController.java
@@ -5,10 +5,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
+	// Controller cho cư dân 
     @GetMapping("/resident/dashboard")
     public String residentDashboard() {
         return "dashboard-resident";
     }
+    
+    @GetMapping("/resident/fees-resident")
+    public String showFeesPageSimplified() {
+        return "fees-resident"; 
+    }
+    
+    @GetMapping("/resident/resident-profile")
+    public String showResidentProfile() {
+    	return "resident-profile";
+    }
+    
+    
+    
     @GetMapping("/staff/dashboard")
     public String staffDashboard() {
         return "dashboard-staff";
