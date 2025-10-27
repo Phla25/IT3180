897e9060158186a219207273b745af6671e9e61a
nguyenLTpk <nguyenngungonvc@gmail.com>
2025-10-27 19:13:16 +0700

    Thêm PhanHoiThongBaoDTO và ThongBaoDTO

---
+++ bluemoon/bluemoon/src/main/java/BlueMoon/bluemoon/models/PhanHoiThongBaoDTO.java
@@ -0,0 +1,37 @@
+package BlueMoon.bluemoon.models;
+
+
+
+import java.time.LocalDateTime;
+
+import BlueMoon.bluemoon.entities.PhanHoiThongBao;
+
+public class PhanHoiThongBaoDTO {
+    private String noiDung;
+    private LocalDateTime thoiGianGui;
+    private String hoVaTenNguoiGui;
+    private String vaiTroNguoiGui;
+
+    public PhanHoiThongBaoDTO(PhanHoiThongBao ph) {
+        this.noiDung = ph.getNoiDung();
+        this.thoiGianGui = ph.getThoiGianGui();
+        this.hoVaTenNguoiGui = ph.getNguoiGui().getHoVaTen();
+        this.vaiTroNguoiGui = ph.getNguoiGui().getVaiTro().toString();
+    }
+    
+    public String getNoiDung() {
+        return noiDung;
+    }
+
+    public LocalDateTime getThoiGianGui() {
+        return thoiGianGui;
+    }
+
+    public String getHoVaTenNguoiGui() {
+        return hoVaTenNguoiGui;
+    }
+
+    public String getVaiTroNguoiGui() {
+        return vaiTroNguoiGui;
+    }
+}
